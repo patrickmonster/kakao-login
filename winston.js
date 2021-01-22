83d9e5ae0d073006fe3a88eb69ca460fff7f1248
Soung jin Ryu <rsj11201@gmail.com>
2021-01-22 21:04:46 +0900

    Update winston.js

--- winston.js
+++ winston.js
@@ -1,18 +1,13 @@
 const winston = require("winston")
 const winstonDaily = require("winston-daily-rotate-file")
 
-const logDir = 'logs';  // logs 디렉토리 하위에 로그 파일 저장
+const logDir = 'logs';
 const { combine, timestamp, printf } = winston.format;
 
-// Define log format
 const logFormat = printf(info => {
   return `${info.timestamp} ${info.level}: ${info.message}`;
 });
 
-/*
- * Log Level
- * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
- */
 const logger = winston.createLogger({
   format: combine(
     timestamp({
@@ -37,6 +32,15 @@ const logger = winston.createLogger({
       dirname: logDir + '/error',  // error.log 파일은 /logs/error 하위에 저장
       filename: `%DATE%.error.log`,
       maxFiles: 30,
+      zippedArchive: true,
+    }),
+    // http 레벨 로그를 저장할 파일 설정
+    new winstonDaily({
+      level: 'http',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/http',
+      filename: `%DATE%.http.log`,
+      maxFiles: 30,  // 30일치 로그 파일 저장
       zippedArchive: true,
     }),
   ],
